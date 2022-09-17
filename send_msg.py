15e75f902945d96cc449a459844de17c47a5ec9a
Fidel José Perez Alcantares De la guardia <78449536+Fidnix@users.noreply.github.com>
2022-09-16 19:01:14 -0500

    Add files via upload
    El archivo webbrowser.py no es de mi autoría, pero no se puede instalar ya con pip creo, por eso lo envío aquí

---
+++ send_msg.py
@@ -0,0 +1,26 @@
+import pyautogui as pg, webbrowser as web, time as tm
+from random import randint
+import keyboard
+
+# Nota imporatnte: debe estar abierto tu ws en el navegador predeterminado
+
+#-----------------------------------
+# Variables
+
+tiempo_de_espera_para_que_cargue_la_pagina = 5 # El tiempo depende de tu internet
+numero_de_telefono = "+51941014571" #<- spameen ese numero
+numero_de_mensajes = 100 # No exageres, porque no tiene boton de pausa
+
+mensaje_a_mandar = ["a"] # Es un arreglo de mensajes, del cual se envian mensajes aleatoriamente
+#-----------------------------------
+
+web.open(f"https://web.whatsapp.com/send?phone={numero_de_telefono}")
+
+tm.sleep(tiempo_de_espera_para_que_cargue_la_pagina)
+
+for i in range(1,numero_de_mensajes):
+
+    numero_random = randint(0,len(mensaje_a_mandar)-1)
+    pg.write(mensaje_a_mandar[numero_random])
+    pg.press("enter")
+    # Esto aun no tiene un boton de pausa, pero pronto lo tendra ;3... espero
